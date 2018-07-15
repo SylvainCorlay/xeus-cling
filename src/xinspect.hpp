ab9bce7830776af9458f2d2316885ed7ad43891d
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-07-15 13:51:14 -0500

    parseresult

--- src/xinspect.hpp
+++ src/xinspect.hpp
@@ -252,6 +252,7 @@ namespace xcpp
                 pugi::xml_document doc;
                 pugi::xml_parse_result result = doc.load_file(filename.c_str());
                 std::cout << "Parse result: " << result << std::endl;
+                std::cout << "Description: " << result.description() << std::endl;
                 for (auto c : check)
                 {
                     node_predicate predicate{c, find_string};
